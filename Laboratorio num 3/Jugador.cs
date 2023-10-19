eff0b53163ba1367c05bdff70e5126c80bbe573d
David Gutierrez <DavidGutierrezcor@hotmail.com>
2023-10-19 00:28:31 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio num 3/Jugador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_num_3
+{
+    internal class Jugador
+    {
+
+
+
+    }
+}
